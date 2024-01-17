fd966e9bd4a2ba8f782320e0d495240ee775846c
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2024-01-17 21:24:34 +0100

    RISOLTO

--- app/src/main/java/com/example/animalapp/Fragment/Segnalazioni_Fragment.java
+++ app/src/main/java/com/example/animalapp/Fragment/Segnalazioni_Fragment.java
@@ -100,15 +100,13 @@ public class Segnalazioni_Fragment extends Fragment {
             cbUtente.setChecked(true);
         }
 
-        StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(segnalazioni.imgSegnalazione);
+      /*  StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(segnalazioni.imgSegnalazione);
         storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 Glide.with(imgSegnalzioni.getContext()).load(uri).circleCrop().into(imgSegnalzioni);
             }
-        });
-
-       */
+        });*/
 
 
         // Inflate the layout for this fragment
