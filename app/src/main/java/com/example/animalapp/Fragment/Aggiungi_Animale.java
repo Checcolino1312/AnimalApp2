650577e9c8a8910f7f34b2693e3a6b41270a4f68
Giusepper1807 <g.rizzitelli4@studenti.uniba.it>
2024-02-02 11:54:49 +0100

    Commit firma apk

--- app/src/main/java/com/example/animalapp/Fragment/Aggiungi_Animale.java
+++ app/src/main/java/com/example/animalapp/Fragment/Aggiungi_Animale.java
@@ -1,14 +1,11 @@
 package com.example.animalapp.Fragment;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,19 +20,14 @@ import androidx.fragment.app.Fragment;
 import com.example.animalapp.Home_Ente_Activity;
 import com.example.animalapp.Home_Veterinario_Activity;
 import com.example.animalapp.MainActivity;
-
 import com.example.animalapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
 import java.util.HashMap;
-
 import java.util.Random;
 
 public class Aggiungi_Animale extends Fragment {
